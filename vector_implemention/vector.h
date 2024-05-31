9c09ab3b3bea5c3f6e9c6bdf73621d2440887bc5
moamen <moamenalali748117@hotmail.com>
2024-05-31 05:23:43 +0300

    added two constructors and a destructor

--- vector_implemention/vector.h
+++ vector_implemention/vector.h
@@ -3,11 +3,23 @@
 
 #include <memory>
 
-template<typename T, typename A = std::allocator<T>>
+template<typename T>
 class vector {
 	int sz;
-	A allocator;
+	std::allocator<T> allocator;
+	T* elem;
+	int space;
+public:
+	vector() : sz{ 0 }, elem{ nullptr }, space{ 0 } {}
 
+	vector(int r, const T& def = T()) : sz{ r }, elem{ allocator.allocate(r) }, space{ r } {
+		for (int i = 0; i < r; ++i) allocator.construct(&elem[i], def);
+	}
+
+	~vector() {
+		for (int i = 0; i < sz; ++i) allocator.destroy(&elem[i]);
+		allocator.deallocate(elem, space);
+	}
 };
 
 #endif
